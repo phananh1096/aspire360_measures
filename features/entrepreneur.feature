5a06fb8bf2f6cba7b0dfe54ee9d93369f2e0b4fe
AumUpadhyay <aumspeaks@gmail.com>
2021-03-13 11:39:48 -0800

    added selenium testing

--- features/entrepreneur.feature
+++ features/entrepreneur.feature
@@ -1,9 +1,24 @@
 Feature: setting up a new entrepreneur
 
-  Scenario: create an entrepreneur
-     Given we have Aspire360 installed
-      When we create a new entrepreneur
-      Then the entrepreneur should have a name
-      Then the entrepreneur should have a user_id
-      Then the entrepreneur should not have surveys
-      Then the entrepreneur should not have investors
+  Scenario: user wants to start and setup Aspire360 app
+      Given we have odoo installed
+      When we go to home screen
+      Then we can go to Aspire360
+
+  Scenario: user enters home page
+      Given we have Aspire360 installed
+      When we go to home screen
+      Then we go to signin
+      Then we log in
+  
+  Scenario: user fails survey
+      Given we are on the home page
+      When we go to surveys
+      Then we go to fundraising survey
+      Then we fail the survey
+  
+  Scenario: user passes survey
+      Given we are on the home page
+      When we go to surveys
+      Then we go to fundraising survey
+      Then we pass the survey
--- features/steps/entrepreneur.py
+++ features/steps/entrepreneur.py
@@ -1,32 +1,114 @@
 from behave import *
-from models.models import Entrepreneurs
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
 
 use_step_matcher("re")
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_BIN = os.path.join(PROJECT_ROOT, "usr/local/bin/chromedriver")
+
+chrome_path = r'/usr/local/bin/chromedriver' #path from 'which chromedriver'
+driver = webdriver.Chrome(executable_path = chrome_path)
+
+@given('we have odoo installed')
+def step_impl(context):
+    try:
+        driver.get("http://localhost:8069")
+    except:
+        assert False
+    assert True
+
+@when('we go to home screen')
+def step_impl(context):
+    try:
+        driver.get("http://localhost:8069")
+        #assert "Homepage!" in driver.page_source
+    except:
+        assert False
+    assert True
+
+@then('we can go to Aspire360')
+def step_impl(context):
+    try:
+        driver.get("http://localhost:8069/aspire360measures")
+    except:
+        assert False
+    assert True
+
 @given('we have Aspire360 installed')
 def step_impl(context):
-    pass
+    try:
+        driver.get("http://localhost:8069/aspire360measures")
+    except:
+        assert False
+    assert True
 
-@when('we create a new entrepreneur')
+@then('we go to signin')
 def step_impl(context):
-    assert True is not False
+    try:
+        driver.get("http://localhost:8069/web/login")
+    except:
+        assert False
+    assert True
 
-@then('the entrepreneur should have a name')
+@then('we log in')
 def step_impl(context):
-    #print(field)
+    email_input = driver.find_element_by_id("login")
+    email_input.send_keys('admin')
+    email_input.send_keys(Keys.ENTER)
+    password_input = driver.find_element_by_id("password")
+    password_input.send_keys('admin')
+    password_input.send_keys(Keys.ENTER)
+    assert True
+
+@given('we are on the home page')
+def step_impl(context):
+    try:
+        driver.get("http://localhost:8069")
+    except:
+        assert False
+    assert True
+
+@when('we go to surveys')
+def step_impl(context):
+    apps = driver.find_element_by_id("oe_applications")
+    apps.click()
+    try:
+        surveys_button = driver.find_element_by_partial_link_text("Surveys")
+        surveys_button.click()
+    except:
+        driver.get("http://localhost:8069/web#action=147&model=survey.survey&view_type=kanban&cids=1&menu_id=102")
     assert True is not False 
 
-@then('the entrepreneur should have a user_id')
+@then('we go to fundraising survey')
 def step_impl(context):
-    #print(field)
-    assert True is not False 
+    try:
+        driver.get("http://localhost:8069/aspire360measures/survey/fundraise")
+        assert "Readiness to Fundraise Assessment" in driver.page_source
+    except:
+        assert False
+    assert True
 
-@then('the entrepreneur should not have surveys')
+@then('we fail the survey')
 def step_impl(context):
-    #print(field)
-    assert True is not False 
+    python_button = driver.find_element_by_class_name("o_connected_user")
+    python_button.send_keys(Keys.ENTER)
+    wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='wrong_answer']"))).click()
+    python_button = driver.find_element_by_class_name("o_connected_user")
+    python_button.send_keys(Keys.ENTER)
+    #python_button.click()
+    assert True
 
-@then('the entrepreneur should not have investors')
+@then('we pass the survey')
 def step_impl(context):
-    #print(field)
-    assert True is not False 
+    python_button = driver.find_element_by_class_name("o_connected_user")
+    python_button.send_keys(Keys.ENTER)
+    wait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='right_answer']"))).click()
+    python_button = driver.find_element_by_class_name("o_connected_user")
+    python_button.send_keys(Keys.ENTER)
+    #python_button.click()
+    assert True
